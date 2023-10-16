620ace3c57d7a58289e30811cb2c180fa2b91cf4
Marcel <marcelpodsiadlo.business@gmail.com>
2023-10-16 23:02:01 +0200

    Add styles, change some code and create a comments

--- script/script.py
+++ script/script.py
@@ -2,28 +2,35 @@ import tkinter as tk
 import os
 
 # Styles
-
 custom_font = ('Arial', 17)
 
 # All necessary paths 
 user_folder_path = 'users'
 folders_path = 'users/'
 
+# This function can clear all content on screen
+def clear_screen():
+    for widget in main_box.winfo_children():
+        widget.destroy()
+
+# This function create users accounts
 def create_user_interface():
     clear_screen()
-    add_user_label = tk.Label(main_box, text="Podaj nazwę użytkownika:")
-    username_entry = tk.Entry(main_box)
-    submit_add_user_button = tk.Button(main_box, text="Dodaj", command=lambda: create_user(username_entry.get()))
+    # Get username from user and 
+    add_user_label = tk.Label(main_box, text="Podaj nazwę użytkownika:", font=custom_font)
+    username_entry = tk.Entry(main_box, font=custom_font)
+    submit_add_user_button = tk.Button(main_box, text="Dodaj", command=lambda: create_user(username_entry.get()), font=custom_font)
     add_user_comment_label = tk.Label(main_box, text="", font=custom_font)
 
     add_user_label.pack()
     username_entry.pack()
     submit_add_user_button.pack()
     add_user_comment_label.pack()
+    
+    # Function that checks if user doesn't filled input, username doesn't exists or account has the same name
     def create_user(username):
         if not username:
-            return 
-
+            add_user_comment_label.config(text="Błąd! Wpisz poprawnie dane")
         if username_exists(username):
             add_user_comment_label.config(text=f"Użytkownik {username} już istnieje")
         else:
@@ -33,15 +40,12 @@ def create_user_interface():
         files = os.listdir(user_folder_path)
         return username in files
     
-
+# Create account folder 
 def create_user_folder(username):
     user_folder = os.path.join(user_folder_path, username)
     os.mkdir(user_folder)
 
-def clear_screen():
-    for widget in main_box.winfo_children():
-        widget.destroy()
-
+# Show accounts
 def display_users_folder():
     clear_screen()
     user_label = tk.Label(main_box, text="Użytkownicy:")
@@ -51,8 +55,8 @@ def display_users_folder():
         user_button = tk.Button(main_box, text=folder, command=lambda folder=folder: display_user_data(folder), borderwidth=2, relief="ridge")
         user_button.pack()
 
+# Show accounts folder
 def display_user_data(username):
-    # Clear screen after open account folder
     clear_screen()
 
     user_label = tk.Label(main_box, text=f"Profil użytkownika: {username}")
@@ -61,8 +65,9 @@ def display_user_data(username):
     dates = os.listdir(os.path.join(user_folder_path, username))
     for date in dates:
         date_button = tk.Button(main_box, text=date, command=lambda date=date: display_date_data(username, date), borderwidth=2, relief="ridge")
-        date_button.pack(side="top-left")
+        date_button.pack(side="left")
 
+# Show account data
 def display_date_data(username, date):
     date_label = tk.Label(main_box, text=f"Data: {date}")
     date_label.pack()
@@ -72,7 +77,7 @@ def display_date_data(username, date):
         file_label = tk.Label(main_box, text=file)
         file_label.pack()
 
-# Main interface - users account
+# Main interface
 root = tk.Tk()
 root.title("Norma pracy")
 
--- script/setup.py
+++ script/setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 setup (
   name = 'Manager',
-  version = '3.5.1',
-  description = "Zmiana styli",
+  version = '3.5.2',
+  description = "Update styles, add comments and change some code",
   executables = [Executable('script.py')]
 )
